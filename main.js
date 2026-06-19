84de00c0d3b32696d239d3762b52c9d3e13e6fbb
utente <mario.manfredi64@gmail.com>
2026-06-19 09:20:17 +0200

    Ordine crescente

--- main.js
+++ main.js
@@ -3,5 +3,9 @@ let array = [3, 7, -2, 5, 8, 1, 2, 5, 6, -4, 12, 25];
 
 
 // ordine decresente
-let ordineC = array.sort( (a, b)=> b - a );
+let ordineD = array.sort( (a, b)=> b - a );
+console.log(ordineD);
+
+// ordine crescente
+let ordineC = array.sort ( (a, b)=> a -b );
 console.log(ordineC);
